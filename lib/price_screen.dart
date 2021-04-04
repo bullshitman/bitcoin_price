df9f7746505cba34a2a3885c3f60341e792d3c30
Eugene <sokol.joe@gmail.com>
2021-04-04 20:20:48 +0300

    test dropdown widget

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'coin_data.dart';
 
-class PriceScreen extends StatelessWidget {
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,8 +47,26 @@ class PriceScreen extends StatelessWidget {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             child: DropdownButton<String>(
-              items: [],
-              onChanged: null,
+              value: selectedCurrency,
+              items: [
+                DropdownMenuItem(
+                  child: Text('USD'),
+                  value: 'USD',
+                ),
+                DropdownMenuItem(
+                  child: Text('EUR'),
+                  value: 'EUR',
+                ),
+                DropdownMenuItem(
+                  child: Text('GBP'),
+                  value: 'GBP',
+                ),
+              ],
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrency = value;
+                });
+              },
             ),
           ),
         ],
